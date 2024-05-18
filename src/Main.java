4fac0b219c13082fd8b6e490120daffac2364901
RDuggan14 <168575791+RDuggan14@users.noreply.github.com>
2024-05-18 19:58:43 -0400

    smth

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args){
         Window Frame = new Window();
         Window.main(Frame);
-        System.out.printf("Hello and welcome!");
     }
 
 
